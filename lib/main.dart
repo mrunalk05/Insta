c1017f2039428419381f411bffe6740872ccdd46
mrunal <mrunalkhade35@gmail.com>
2022-10-28 09:12:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:insta/screens/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyD-Atx0OYn6tKr5tKDKZbddSNkfkwGCh1Y",
+      appId: "1:600961526071:web:32147287ba59c843f192b2",
+      messagingSenderId: "600961526071",
+      projectId: "insta-995ec",
+      storageBucket: "insta-995ec.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Login(),
+    );
+  }
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:insta/utils/colors.dart';
+import 'package:insta/widgets/textfield.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passcontroller = TextEditingController();
+  final TextEditingController usercontroller = TextEditingController();
+  final TextEditingController conformcontroller = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    emailcontroller.dispose();
+    passcontroller.dispose();
+    conformcontroller.dispose();
+    usercontroller.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(child: Container(), flex: 2),
+            TextFieldInput(
+                textEditingController: emailcontroller,
+                hintText: "Enter Your Email",
+                textInputType: TextInputType.emailAddress),
+            const SizedBox(height: 64),
+            TextFieldInput(
+              textEditingController: passcontroller,
+              hintText: "Enter Your Password",
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            TextFieldInput(
+              textEditingController: conformcontroller,
+              hintText: "Conform Your Password",
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            TextFieldInput(
+              textEditingController: usercontroller,
+              hintText: "Enter Your Username",
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            InkWell(
+              child: Container(
+                child: Text('Log In'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                  ),
+                  color: blueColor,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            Row(
+              children: [
+                Container(
+                  child: Text("Already have Account"),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
